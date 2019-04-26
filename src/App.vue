27691c3c77bda7b0c5fb34fc883d8ffecc9fffb3
sarasnmsp21 <saraswati.gauda@wohlig.com>
2019-04-26 15:52:14 +0530

    first installation

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+      <router-link to="/about">About</router-link>|
+      <router-link to="/sport">sport</router-link>
+    </div> -->
     <router-view />
   </div>
 </template>
+
+/* cyrillic */
+@font-face { font-family: 'Oswald'; font-style: normal;
+font-weight: 300; src: local('Oswald Light'), local('Oswald-Light'),
+url(https://fonts.gstatic.com/s/oswald/v17/TK3hWkUHHAIjg75-sh0Tvs9CE5Q.woff2)
+format('woff2'); unicode-range: U+0400-045F, U+0490-0491, U+04B0-04B1, U+2116; }
 
 <style lang="scss">
 #app {
--- src/main.js
+++ src/main.js
@@ -2,6 +2,82 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+
+Vue.use(BootstrapVue);
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import {
+  faSquare,
+  faCalendar,
+  faCircle,
+  faPencilAlt,
+  faUserSecret,
+  faCaretDown,
+  faTh,
+  faDumbbell,
+  faEnvelopeOpenText,
+  faPhone,
+  faEnvelope,
+  faUser,
+  faStar,
+  faStarHalfAlt
+} from "@fortawesome/free-solid-svg-icons"; //fas fa
+
+import {
+  faLinkedinIn,
+  faFacebookSquare,
+  faTwitter,
+  faGoogle,
+  faYoutube,
+  faInstagram,
+  faDribbble,
+  faTelegramPlane,
+  faFacebookF,
+  faPinterestP,
+  faApple
+} from "@fortawesome/free-brands-svg-icons"; //fab
+
+import { faCommentAlt, faClock } from "@fortawesome/free-regular-svg-icons"; //far
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(
+  faCircle,
+  faPencilAlt,
+  faCalendar,
+  faSquare,
+  faUserSecret,
+  faCaretDown,
+  faTh,
+  faDumbbell,
+  faEnvelopeOpenText,
+  faPhone,
+  faEnvelope,
+  faUser,
+  faStar,
+  faStarHalfAlt
+); //fas
+
+library.add(faCommentAlt, faClock); //far
+
+library.add(
+  faLinkedinIn,
+  faFacebookSquare,
+  faTwitter,
+  faGoogle,
+  faYoutube,
+  faInstagram,
+  faDribbble,
+  faTelegramPlane,
+  faFacebookF,
+  faPinterestP,
+  faApple
+); //fab
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
